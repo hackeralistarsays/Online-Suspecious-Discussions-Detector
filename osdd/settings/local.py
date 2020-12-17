539b6b58b4b1cced013df9163bd4485026d0236a
samsonmutangili <samsonkimole@gmail.com>
2020-12-17 06:52:43 +0300

    second commit

---
+++ osdd/settings/local.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'o$&)7d6sp_wfe(+e)_o-mnnjg1a8nl6oe%7-s06sn&+2xy+-p('
+
+
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ibrentone.alistar@gmail.com'
+EMAIL_HOST_PASSWORD = 'ArcadiaComplex'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- userauth/forms.py
+++ userauth/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import CustomUser
 from wagtail.users.forms import UserCreationForm, UserEditForm
+from django.utils.translation import gettext_lazy as _
+from django.forms import ModelForm
 
 
 class WagtailUserCreationForm(UserCreationForm):
@@ -12,4 +14,25 @@ class WagtailUserCreationForm(UserCreationForm):
 class WagtailUserEditForm(UserEditForm):
     class Meta(UserEditForm.Meta):
         model = CustomUser
+        widgets = {'date_of_birth': forms.DateInput(attrs={'type':'date'})}
+
+
+
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=30, label=_("First name"))
+    last_name = forms.CharField(max_length=30, label=_("Last name"))
+    display_name = forms.CharField(max_length=30, label=_("Display name"), help_text=_("Will be shown e.g. when commenting."))
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.display_name = self.cleaned_data['display_name']
+        user.save()
+
+
+class CustomUserUpdateForm(ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'display_name', 'date_of_birth', 'address1', 'address2', 'zip_code', 'city', 'country', 'mobile_phone', 'additional_information', 'photo',]
         widgets = {'date_of_birth': forms.DateInput(attrs={'type':'date'})}
